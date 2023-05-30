29653066947752a67a11bdea5d6b2232d8bc1589
Efendo <130039463+Efendo@users.noreply.github.com>
2023-05-30 19:48:46 +0200

    Update Pywext.py

--- Pywext.py
+++ Pywext.py
@@ -1,1 +1,14 @@
-print("hi")
+def HTML(content, HTML_File):
+    print(f"<!DOCTYPE html>\n<html>\n{content}\n</html>", file=open(HTML_File,'w'))
+
+def Head(content):
+    return f"<head>\n{content}\n</head>\n"
+
+def Body(attributes, content):
+    return f"<body {attributes}>\n{content}\n</body>\n"
+    
+def Tag(tag, attributes, content, self_closing):
+    if self_closing:
+        return f"<{tag} {attributes}>"
+    else:
+        return f"<{tag} {attributes}>{content}</{tag}>"
